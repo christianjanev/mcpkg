3213b6f2f16ff6ff1e207a354da2d69c251fa57e
christianjanev <christianjanev7@gmail.com>
2024-07-09 17:41:10 -0500

    Search for modrinth mods

--- modrinth.go
+++ modrinth.go
@@ -39,10 +39,87 @@ type SearchResults struct {
 	Total_hit json.Number
 }
 
-func modrinthSearch(query string) (SearchResults, error) {
+type GetResult struct {
+    Slug                  string `json:"slug"`
+    Title                 string `json:"title"`
+    Description           string `json:"description"`
+    Categories            []string `json:"categories"`
+    Client_side           string `json:"client_side"`
+    Server_side           string `json:"server_side"`
+    Body                  string `json:"body"`
+    Status                string `json:"status"`
+    Requested_status      string `json:"requested_status"`
+    Additional_categories []string `json:"additional_categories"`
+    Issues_url            string `json:"issues_url"`
+    Source_url            string `json:"source_url"`
+    Wiki_url              string `json:"wiki_url"`
+    Discord_url           string `json:"discord_url"`
+    Donation_urls         []string `json:"donation_urls"`
+    Project_type          string `json:"project_type"`
+    Downloads             json.Number `json:"downloads"`
+    Icon_url              string `json:"icon_url"`
+    Color                 json.Number `json:"color"`
+    Thread_id             string `json:"thread_id"`
+    Monetization_status   string `json:"monetization_status"`
+    Id                    string `json:"id"`
+    Team                  string `json:"team"`
+    Body_url              string `json:"body_url"`
+    Moderator_message     string `json:"moderator_message"`
+    Published             string `json:"published"`
+    Updated               string `json:"updated"`
+    Approved              string `json:"approved"`
+    Queued                string `json:"queued"`
+    Followers             json.Number `json:"followers"`
+    License               struct {
+        Id   string `json:"id"`
+        Name string `json:"name"`
+        Url  string `json:"url"`
+    } `json:"license"`
+    Versions       []string `json:"versions"`
+    Game_versions  []string `json:"game_versions"`
+    Loaders        []string `json:"loaders"`
+    Gallery        []struct {
+        Url         string `json:"url"`
+        Featured    bool `json:"featured"`
+        Title       string `json:"string"`
+        Description string `json:"description"`
+        Created     string `json:"created"`
+        Ordering    json.Number `json:"ordering"`
+    } `json:"gallery"`
+}
+
+func modrinthSearch(query string) SearchResults {
 	resp, _ := get(MODRINTH_ENDPOINT + "/search?query=" + query)
 	
-    bodyObj, _ := 
+    bodyObj, _ := io.ReadAll(resp.Body)
+    bodyStr := string(bodyObj)
+    body := SearchResults{}
+
+    err := json.Unmarshal([]byte(bodyStr), &body)
+
+    if err != nil {
+		log.Println(err.Error())
+		log.Fatalln("modrinthSearch() json unmarshal failed.")
+	}
+
+    return body
+}
+
+func modrinthGet(slug string) GetResult {
+    resp, _ := get(MODRINTH_ENDPOINT + "/project/" + slug)
+
+    bodyObj, _ := io.ReadAll(resp.Body)
+    bodyStr := string(bodyObj)
+    body := GetResult{}
+
+    err := json.Unmarshal([]byte(bodyStr), &body)
+
+    if err != nil {
+        log.Println(err.Error())
+		log.Fatalln("modrinthSearch() json unmarshal failed.")
+    }
+
+    return body
 }
 
 func modInstall(args []string) {
@@ -51,4 +128,11 @@ func modInstall(args []string) {
 
 func modSearch(args []string) {
 	log.Println("Searching modrinth mod for " + args[0])
+
+    result := modrinthSearch(args[0])
+
+    for index := 0; index < len(result.Hits); index++ {
+        log.Println(result.Hits[index].Title + " - " + result.Hits[index].Slug)
+    }
+
 }
